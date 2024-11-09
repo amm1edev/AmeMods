5a319f3be16fb88eb4996b2d8eb90021f41d9527
amm1e <88576648+amm1edev@users.noreply.github.com>
2024-11-09 08:09:24 +0600

    Update uselessweb.py

--- uselessweb.py
+++ uselessweb.py
@@ -1,4 +1,4 @@
-# meta developer: @amm1e
+# meta developer: @amm1e & @AmeMods
 
 import requests
 import random
@@ -7,13 +7,18 @@ from .. import loader
 
 @loader.tds
 class uselesswmod(loader.Module):
-    """Бесполезный модуль для вывода бесполезных сайтов."""
+    """Useless mod for useless sites."""
     strings = {"name": "uselessweb"}
+    strings_ru = {"Useless mod for useless sites." = "Бесполезный мод для бесполезных сайтов."}
+    strings_en = {"<emoji document_id=5318759457801385682>👍</emoji> <b>Ваш рандомный сайт</b>:" = "<emoji document_id=5318759457801385682>👍</emoji> <b>Your random site</b>:"}
 
-    @loader.command()
+
+    @loader.command(
+        en_doc = "- Random useless site"
+    )
     async def uselessweb(self, message):
         """- Рандомный бесполезный сайт."""
         response = requests.get('https://gist.githubusercontent.com/quest/07bbc6908f84b50a9fc8/raw/d8983a0723d07203816b78953ff52f07423c808d/uselessweb.json')
         file = response.json()
         uselesswebr = random.choice(file['uselessweb'])
-        await message.edit(f'<emoji document_id=5318759457801385682>👍</emoji> <b>Ваш рандомный сайт</b>:  {uselesswebr}')
+        await message.edit("<emoji document_id=5318759457801385682>👍</emoji> <b>Ваш рандомный сайт</b>:" + uselesswebr)
