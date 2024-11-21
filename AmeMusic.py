f403c7f0ae90e05f686f028b3565f5511a00a330
amm1e <88576648+amm1edev@users.noreply.github.com>
2024-11-21 18:26:30 +0600

    Fix

--- AmeMusic.py
+++ AmeMusic.py
@@ -1,7 +1,6 @@
-#meta developer: @amm1e & @AmeMods
-
 from telethon import events
 from .. import loader, utils
+import asyncio
 
 @loader.tds
 class AmeMusic(loader.Module):
@@ -10,19 +9,15 @@ class AmeMusic(loader.Module):
     strings = {
         "name": "AmeMusic",
         "searching": "<b>🔍 Ищу музыку...</b>",
-        "no_results": "<b>❌ Не удалось найти трек. Попробуйте указать автора.</b>",
+        "no_results": "<b>❌ Произошла ошибка. Попробуйте указать точное название трека, либо трек невозможно найти.</b>",
         "loading": "<b>⏳ Загрузка трека...</b>",
-        "download_error": "<b>❌ Не удалось скачать аудио для песни. Обратитесь в тех.поддержку.</b>",
-        "unexpected_error": "<b>❌ Произошла ошибка. Попробуйте позже.</b>", 
         "enter_name": "<b>❌ Введите название трека </b>"
     }
     strings_en = {
         "name": "AmeMusic",
-        "searching": "<b>🔍 Search music...</b>",
-        "no_results": "<b>❌ Could not find the track, please try again later.</b>",
+        "searching": "<b>🔍 Searching music...</b>",
+        "no_results": "<b>❌ Could not find the track, try entering the correct track name or track author.</b>",
         "loading": "<b>⏳ Downloading track...</b>",
-        "download_error": "<b>❌ Could not download the track.</b>",
-        "unexpected_error": "<b>❌ Error, try again later.</b>", 
         "enter_name": "<b>❌ Enter track name </b>"
     }
 
@@ -38,25 +33,25 @@ class AmeMusic(loader.Module):
         try:
             async with message.client.conversation("@AmeMusicbot") as conv:
                 await conv.send_message(args)
-
                 while True:
                     response = await conv.get_response()
-
                     if "Не удалось найти трек" in response.text:
                         await message.edit(self.strings("no_results"))
-                        return  
-
-                    elif "Загрузка трека" in response.text:
+                        return
+                    
+                    if "Загрузка трека" in response.text:
                         await message.edit(self.strings("loading"))
 
-                    elif "Не удалось скачать аудио" in response.text:
-                        await message.edit(self.strings("download_error"))
+                    if response.media:
+                        await message.client.send_file(message.chat_id, response.media)
+                        await message.delete()
                         return
 
-                    elif response.media:
-                        await message.client.send_message(message.chat_id, response)
-                        await message.delete()
-                        break
+                    await asyncio.sleep(3)
+                    updated_response = await message.client.get_messages(conv.chat_id, ids=response.id)
+                    if updated_response.text != response.text and "Не удалось найти трек" in updated_response.text:
+                        await message.edit(self.strings("no_results"))
+                        return
 
         except Exception as e:
-            await message.edit(self.strings("unexpected_error"))
+            await message.edit(self.strings("no_results"))
